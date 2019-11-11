2136ffaf7fb9212e04586157a455b5180254862a
odajima-ca <odajima_yu_gn@cyberagent.co.jp>
2019-11-11 14:55:22 +0900

    a

--- packages/ptag/src/index.tsx
+++ packages/ptag/src/index.tsx
@@ -1,6 +1,24 @@
+import { ThemeProvider } from "emotion-theming";
 import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const theme = {
+  colors: {
+    background: "black",
+    primary: "tomato"
+  },
+  space: [0, 6, 12, 24, 48],
+  fontSizes: [14, 16, 18, 20, 24],
+  radii: {
+    default: 12
+  }
+};
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
