d996aa71034770203d726c068dfe4ce4854b2fcc
odajima-ca <odajima_yu_gn@cyberagent.co.jp>
2019-11-11 13:39:03 +0900

    a

--- packages/ptag/src/App.tsx
+++ packages/ptag/src/App.tsx
@@ -1,20 +1,50 @@
 import React from "react";
+import Iframe from "react-iframe";
+import { Box, Card, Flex } from "rebass";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Flex flexDirection="row" flexWrap="wrap">
+          <Box p={1} width={[1, 1 / 2, 1 / 3, 1 / 4]} height={256}>
+            <Card bg="gray" height="100%" />
+          </Box>
+          <Box p={1} width={[1, 1 / 2, 1 / 3, 1 / 4]} height={256}>
+            <Card bg="gray" height="100%" />
+          </Box>
+          <Box p={1} width={[1, 1 / 2, 1 / 3, 1 / 4]} height={256}>
+            <Card bg="gray" height="100%" />
+          </Box>
+          <Box p={1} width={[1, 1 / 2, 1 / 3, 1 / 4]} height={256}>
+            <Card bg="gray" height="100%" />
+          </Box>
+          <Box p={1} width={[1, 1/2, 1/3, 1/4]} height={256}>
+            <Iframe
+              url={process.env.REACT_APP_ATAG_HOST}
+              width="100%"
+              height="100%"
+              id="myId"
+              className="myClassname"
+              display="inline"
+              position="relative"
+              frameBorder={0}
+              styles={{ boxSizing: "border-box" }}
+            />
+          </Box>
+          <Box p={1} width={[1, 1 / 2, 1 / 3, 1 / 4]} height={256}>
+            <Card bg="gray" height="100%" />
+          </Box>
+          <Box p={1} width={[1, 1 / 2, 1 / 3, 1 / 4]} height={256}>
+            <Card bg="gray" height="100%" />
+          </Box>
+          <Box p={1} width={[1, 1 / 2, 1 / 3, 1 / 4]} height={256}>
+            <Card bg="gray" height="100%" />
+          </Box>
+          <Box p={1} width={[1, 1 / 2, 1 / 3, 1 / 4]} height={256}>
+            <Card bg="gray" height="100%" />
+          </Box>
+        </Flex>
       </header>
     </div>
   );
--- packages/ptag/src/react-app-env.d.ts
+++ packages/ptag/src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+
+// Process ENV
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly NODE_ENV: "development" | "production" | "test";
+    readonly PUBLIC_URL: string;
+
+    readonly REACT_APP_ATAG_HOST: string;
+  }
+}
