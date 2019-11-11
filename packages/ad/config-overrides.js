12bc7db70992f07d0e4a1b526cede24cf625f39e
odajima-ca <odajima_yu_gn@cyberagent.co.jp>
2019-11-11 21:48:28 +0900

    a

--- packages/ad/config-overrides.js
+++ packages/ad/config-overrides.js
@@ -14,10 +14,8 @@ const multipleEntry = require("react-app-rewire-multiple-entry")([
 module.exports = function override(config, env) {
   multipleEntry.addMultiEntry(config);
 
-  if (process.env.NODE_ENV === "production") {
-    config.output.filename = "static/js/[name].[contenthash:8].js";
-    config.output.chunkFilename = "static/js/[name].chunk.js";
-  }
+  config.output.filename = "static/js/[name].js";
+  config.output.chunkFilename = "static/js/[name].chunk.js";
 
   config.optimization.runtimeChunk = false;
   config.optimization.splitChunks = {
